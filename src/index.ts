57ddc82ca1bc46f05f457b1b7af1e5f668e0ff1d
César Alberca <cesalberca@gmail.com>
2020-09-05 21:22:33 +0200

    Change condition

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import * as functions from 'firebase-functions'
 import { CodeGenerator } from './code-generator'
 
-export const onGameStart = functions.firestore.document('stairs/{stairId}').onUpdate(change => {
+export const onStairCreated = functions.firestore.document('stairs/{stairId}').onUpdate(change => {
   const data = change.after.data() as { code: string } | undefined
   const previousData = change.before.data() as { code: string } | undefined
 
-  if (data?.code === undefined || previousData?.code !== undefined) {
+  if (data?.code !== undefined || previousData?.code !== undefined) {
     return null
   }
 
