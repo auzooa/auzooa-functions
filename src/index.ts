ce4660e7fbec45f5631e8086261edcc473811c53
César Alberca <cesalberca@gmail.com>
2020-09-05 21:31:32 +0200

    Change to on create

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,7 @@
 import * as functions from 'firebase-functions'
 import { CodeGenerator } from './code-generator'
 
-export const onStairCreated = functions.firestore.document('stairs/{stairId}').onUpdate(change => {
-  const data = change.after.data() as { code: string } | undefined
-  console.log({ data })
-
-  if (data?.code !== undefined) {
-    return null
-  }
-
+export const onStairCreated = functions.firestore.document('stairs/{stairId}').onCreate(change => {
   const codeGenerator = new CodeGenerator({
     provide(): number {
       return Math.random()
@@ -16,8 +9,7 @@ export const onStairCreated = functions.firestore.document('stairs/{stairId}').o
   })
 
   const code = codeGenerator.generate()
-  console.log({ code })
-  return change.after.ref.set(
+  return change.ref.set(
     {
       code
     },
